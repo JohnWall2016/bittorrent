e760e5fef92b2e9b3d16bf559f86b92e540b03b8
John Wall <5672618@qq.com>
2018-11-01 21:36:32 +0800

    update

--- BitTorrent/BEncoding.cs
+++ BitTorrent/BEncoding.cs
@@ -8,22 +8,30 @@ namespace BitTorrent.BEncoding
 {
     public static class Bytes
     {
-        public const string _encodeName = "utf8";
+        public const string EncodeName = "utf-8";
 
-        public static byte[] ToBytes(this int i, string encodeName = _encodeName)
+        public static byte[] ToBytes(this int i, string encodeName = EncodeName)
         => Encoding.GetEncoding(encodeName).GetBytes(i.ToString());
 
-        public static byte[] ToBytes(this long i, string encodeName = _encodeName)
+        public static byte[] ToBytes(this long i, string encodeName = EncodeName)
         => Encoding.GetEncoding(encodeName).GetBytes(i.ToString());
 
-        public static byte[] ToBytes(this string str, string encodeName = _encodeName)
+        public static byte[] ToBytes(this string str, string encodeName = EncodeName)
         => Encoding.GetEncoding(encodeName).GetBytes(str);
 
-        public static string ToString(this byte[] bytes, string encodeName = _encodeName)
+        public static string ToString(this byte[] bytes, string encodeName = EncodeName)
         => Encoding.GetEncoding(encodeName).GetString(bytes);
 
-        public static byte[] Copy(IEnumerable<byte> src, int count) 
-        => src.Take(count).ToArray();
+        public static string Dump(this byte[] bytes)
+        {
+            var builder = new StringBuilder();
+            for (var i = 0; i < bytes.Length; i++)
+            {
+                if (i > 0) builder.Append(" ");
+                builder.Append(bytes[i].ToString("X"));
+            }
+            return builder.ToString();
+        }
 
         public static byte[] Join(params byte[][] bytes)
         {
@@ -42,22 +50,80 @@ namespace BitTorrent.BEncoding
 
     public class String : IDumpable
     {
-        byte[] _data = null;
+        MemoryStream _data = new MemoryStream();
 
-        public String(IEnumerable<byte> src, int count)
-        => _data = Bytes.Copy(src, count);
+        public void AddString(byte[] buffer, int offset, int count)
+        => _data.Write(buffer, offset, count);
 
         public byte[] Dump()
-        => Bytes.Join(_data.Length.ToBytes(), ":".ToBytes(), _data);
+        => Bytes.Join(_data.Length.ToBytes(), ":".ToBytes(), _data.GetBuffer());
+
+        public string ToString(string encodeName = Bytes.EncodeName)
+        => Encoding.GetEncoding(encodeName).GetString(_data.GetBuffer());
     }
 
     public class Number : IDumpable
     {
-        long _number;
+        MemoryStream _data = new MemoryStream();
 
-        public Number(string number) => _number = long.Parse(number);
+        public void AddDigit(byte digit)
+        => _data.WriteByte(digit);
 
         public byte[] Dump()
-        => Bytes.Join("i".ToBytes(), _number.ToBytes(), "e".ToBytes());
+        => Bytes.Join("i".ToBytes(), _data.GetBuffer(), "e".ToBytes());
+
+        public long ToLong()
+        => long.Parse(Encoding.ASCII.GetString(_data.GetBuffer()));
+    }
+
+    public class List : IDumpable
+    {
+        List<IDumpable> _list = new List<IDumpable>();
+
+        public void AddItem(IDumpable item)
+        => _list.Add(item);
+
+        public byte[] Dump()
+        {
+            var stream = new MemoryStream();
+            stream.Write("l".ToBytes());
+            foreach (var l in _list)
+            {
+                stream.Write(l.Dump());
+            }
+            stream.Write("e".ToBytes());
+            return stream.GetBuffer();
+        }
+    }
+
+    public class Dictionary : IDumpable
+    {
+        List<IDumpable> _keys = new List<IDumpable>();
+        Dictionary<IDumpable, IDumpable> _dir = new Dictionary<IDumpable, IDumpable>();
+
+        public void AddPair(IDumpable key, IDumpable value)
+        {
+            if (_keys.Contains(key)) return;
+            _keys.Add(key);
+            _dir[key] = value;
+        }
+
+        public byte[] Dump()
+        {
+            var stream = new MemoryStream();
+            stream.Write("d".ToBytes());
+            foreach (var key in _keys)
+            {
+                stream.Write(key.Dump());
+                stream.Write(_dir[key].Dump());
+            }
+            stream.Write("e".ToBytes());
+            return stream.GetBuffer();
+        }
+    }
+
+    public class TorrentFile
+    {
+        
     }
 }
---
+++ BitTorrentTest/BEncodingTest.cs
@@ -0,0 +1,23 @@
+using System;
+using Xunit;
+using Xunit.Abstractions;
+using BitTorrent.BEncoding;
+
+namespace BitTorrentTest
+{
+    public class BEncodingTest
+    {
+        private readonly ITestOutputHelper output;
+
+        public BEncodingTest(ITestOutputHelper output) => this.output = output;
+
+        [Fact]
+        public void Test1()
+        {
+            var bytes = 12345.ToBytes();
+            output.WriteLine("{0}:[{1}]", bytes.Length, bytes.Dump());
+            bytes = "i".ToBytes();
+            output.WriteLine("{0}:[{1}]", bytes.Length, bytes.Dump());
+        }
+    }
+}
